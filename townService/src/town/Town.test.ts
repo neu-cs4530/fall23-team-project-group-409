5b7ec100fa763c92582407d65891e494a67f146b
Brian Kapusta <bkapusta2@gmail.com>
2023-11-26 18:42:24 -0500

    removed unknown to remove error?

--- townService/src/town/Town.test.ts
+++ townService/src/town/Town.test.ts
@@ -472,7 +472,6 @@ describe('Town', () => {
       function disconnectPlayer(playerToLeave: MockedPlayer) {
         // Call the disconnect event handler
         const disconnectHandler = getEventListener(playerToLeave.socket, 'disconnect');
-        disconnectHandler('unknown');
       }
       it("Invalidates the players's session token", async () => {
         const token = player.sessionToken;
