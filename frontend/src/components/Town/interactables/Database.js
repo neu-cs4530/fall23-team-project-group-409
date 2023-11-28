50eda3919356a05d0411f9e94f000d40578fb2fc
Drew <drewi@Drews-MacBook-Air.local>
2023-11-28 13:23:10 -0500

    Changed .env file to have a functioning variable

--- frontend/src/components/Town/interactables/Database.js
+++ frontend/src/components/Town/interactables/Database.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 
 export const getAllPlayersInTown = async data => {
-  console.log(process.env.DATABASE_URL);
+  console.log(process.env.NEXT_PUBLIC_TOWN_DATABASE_URL);
   console.log(process.env.NEXT_PUBLIC_TOWNS_SERVICE_URL);
-  const response = await axios.get(`http://localhost:4000/api/bytown/${data}`);
+  const response = await axios.get(
+    `${[process.env.NEXT_PUBLIC_TOWN_DATABASE_URL]}/api/bytown/${data}`,
+  );
   const output = response.data;
   return output;
 };
