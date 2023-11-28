01e663343f00de6f33481ee004bc66a49f094534
Andrew Lee <atandrewlee@gmail.com>
2023-11-28 14:00:19 -0500

    remove console.log

--- frontend/src/components/Town/interactables/Database.js
+++ frontend/src/components/Town/interactables/Database.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 
 export const getAllPlayersInTown = async data => {
-  console.log(process.env.NEXT_PUBLIC_TOWN_DATABASE_URL);
-  console.log(process.env.NEXT_PUBLIC_TOWNS_SERVICE_URL);
   const response = await axios.get(
     `${[process.env.NEXT_PUBLIC_TOWN_DATABASE_URL]}/api/bytown/${data}`,
   );
