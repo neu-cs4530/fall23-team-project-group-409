d1549c1def62a2d9a98409a4122cb5d08a074313
Andrew Lee <atandrewlee@gmail.com>
2023-11-28 20:24:17 -0500

    trying

--- townService/src/town/Database.js
+++ townService/src/town/Database.js
@@ -1,36 +1,58 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-useless-return */
 import axios from 'axios';
 
 export const writeGame = async data => {
-  const response = await axios.post(`${process.env.DATABASE_URL}/api/games`, data);
-  const output = response.data;
-  return output;
+  try {
+    const response = await axios.post(`${process.env.DATABASE_URL}/api/games`, data);
+    const output = response.data;
+    return output;
+  } catch (error) {
+    return;
+  }
 };
 
 export const getPlayerInfo = async userID => {
-  const response = await axios.get(`${process.env.DATABASE_URL}/api/users/${userID}`);
-  const user = response.data[0];
-  return user;
+  try {
+    const response = await axios.get(`${process.env.DATABASE_URL}/api/users/${userID}`);
+    const user = response.data[0];
+    return user;
+  } catch (error) {
+    return;
+  }
 };
 
 export const editPlayerInfo = async (userID, newElo, newWins, newLosses, newTies) => {
-  const response = await axios.put(`${process.env.DATABASE_URL}/api/userselo/${userID}`, {
-    elo: newElo,
-    wins: newWins,
-    losses: newLosses,
-    ties: newTies,
-  });
-  const user = response.data;
-  return user;
+  try {
+    const response = await axios.put(`${process.env.DATABASE_URL}/api/userselo/${userID}`, {
+      elo: newElo,
+      wins: newWins,
+      losses: newLosses,
+      ties: newTies,
+    });
+    const user = response.data;
+    return user;
+  } catch (error) {
+    return;
+  }
 };
 
 export const addPlayer = async data => {
-  const response = await axios.post(`${process.env.DATABASE_URL}/api/users`, data);
-  const user = response.data;
-  return user;
+  try {
+    const response = await axios.post(`${process.env.DATABASE_URL}/api/users`, data);
+    const user = response.data;
+    return user;
+  } catch (error) {
+    return;
+  }
 };
 
 export const getAllPlayersFromTown = async data => {
-  const response = await axios.get(`${process.env.DATABASE_URL}/api/bytown/${data}`);
-  const output = response.data;
-  return output;
+  try {
+    const response = await axios.get(`${process.env.DATABASE_URL}/api/bytown/${data}`);
+    const output = response.data;
+    return output;
+  } catch (error) {
+    return;
+  }
 };
